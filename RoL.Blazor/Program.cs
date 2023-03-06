bac6840f178abfe4971077a26e48265284b6ee9d
Vlad <vladis.osminin@gmail.com>
2023-03-06 19:42:23 +0300

    changed the way of using Snackbar

--- RoL.Blazor/Program.cs
+++ RoL.Blazor/Program.cs
@@ -1,11 +1,25 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor;
 using RoL.Blazor;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+// Adding services to work with MudBlazor
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomLeft;
+    config.SnackbarConfiguration.PreventDuplicates = false;
+    config.SnackbarConfiguration.NewestOnTop = false;
+    config.SnackbarConfiguration.ShowCloseIcon = true;
+    config.SnackbarConfiguration.VisibleStateDuration = 50000;
+    config.SnackbarConfiguration.HideTransitionDuration = 500;
+    config.SnackbarConfiguration.ShowTransitionDuration = 500;
+    config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+});
 
 builder.Services.AddScoped(sp => new HttpClient(new HttpClientHandler()) {BaseAddress = new Uri("http://servers.mix3.ru:6969/api/v1/")});
 builder.Services.AddMudServices();
